7114ac99edf036c0646e01c55dce2002ddcd9351
Robin Kelsick II <robinkelsick@gmail.com>
2023-07-29 17:12:39 -0400

    added about me component. Need to add styling and pictures.

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./styles/page.module.css";
 import HomePage from "../components/HomePage";
 import "node_modules/react-toastify/dist/ReactToastify.css";
+import AboutMe from "../components/AboutMe";
 
 export default function Home() {
   return (
     <div className="main">
-      <HomePage />
+      {/* <HomePage /> */}
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../app/styles/aboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div className="about-me">
+      <div className="title">
+        <h1>About Me</h1>
+      </div>
+      <div className="text">
+        <p className="paragraph">
+          When life give me lemons, I make lemonade. I'm not really here to talk
+          about how tough life my life was or to prove that I didn't jave it
+          easy, however everyone should know that it was an integral part about
+          why I ended up a web developer. When I was in high school, I really
+          had no idea with my life so I just joined the Marine Corps. I was at
+          bootcamp less than 48 hours after my high school graduation. The same
+          story is how I became a developer; again, after college I had no idea
+          what I wanted to do with my life at that point and the rest is
+          history.
+        </p>
+        <p className="paragraph">
+          Two years ago I entered Awesome Inc's bootcamp and began my journey in
+          full-stack web development, primarily dealing with the MERN Stack,
+          primarily in the field of intricate data websites. Since then it has
+          been a relentless pursue of education and self-improvement.
+        </p>
+        <p className="paragraph">
+          My passions outside of tech include basketball, soccer, video games,
+          nd music. I also love gaming with my forever gaming buddy, my fiancee,
+          Mollie. We stay with our 3 beautiful children (2 cats, and a Guinea
+          Pig) Yetta, Sampson, and Orissa. My ultimate goals are to set an
+          example for other black people in tech, help people build the website
+          of their dreams, learn as much as I possibly can, and look for ways to
+          make learning in technology more accessible for those who don't have
+          much. Feel free to explore my portfolio and have a little peek into
+          who I am in my world where being a bandgeek and a developer converge.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
